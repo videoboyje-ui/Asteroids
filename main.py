11b2c68207dfe997754fb69ae5d4f071c5be124c
videoboyje-ui <videoboyje@gmail.com>
2025-11-17 18:52:26 -0500

    Description: Got game window working with logging.
     Changes to be committed:
    	modified:   .gitignore
    	new file:   constants.py
    	new file:   logger.py
    	modified:   main.py

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-def main():
-    print("Hello from asteroids!")
+import pygame
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from logger import log_state
 
+def main():
+    pygame.init()
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # Game loop
+    while True:
+        log_state()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+        screen.fill("black")
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
